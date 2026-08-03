4c0e39ea65280aeec9c5eaaa478e8c9024c2cf4c
ruth-chirwa <chirwaruth647@gmail.com>
2026-08-03 17:29:41 +0200

    chore: update project structure

--- menu/food_menu/views.py
+++ menu/food_menu/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, welcome to the Food Menu App!")
+
+
+def item(request):
+    return HttpResponse("This is the item view.")
--- menu/menu/settings.py
+++ menu/menu/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "food_menu",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
