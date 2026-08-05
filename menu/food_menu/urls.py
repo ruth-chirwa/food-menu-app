6e14db1403abe7725b33b6b1459cb7958ccf93f4
ruth-chirwa <chirwaruth647@gmail.com>
2026-08-05 21:08:47 +0200

    add logic to edit and delete buttons

--- menu/food_menu/urls.py
+++ menu/food_menu/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<int:item_id>/', views.detail, name='detail'),
     path('add/', views.create_food_item, name='create_food_item'),
+    path('update/<int:item_id>/', views.update_food_item, name='update_food_item'),
+    path('delete/<int:item_id>/', views.delete_food_item, name='delete_food_item'),
 ]
--- menu/food_menu/views.py
+++ menu/food_menu/views.py
@@ -28,3 +28,23 @@ def create_food_item(request):
     form = FoodItemForm()
     context = {'form': form}
   return render(request, 'food_menu/create_item.html', context)
+
+def update_food_item(request, item_id):
+    food_item = FoodItem.objects.get(id=item_id)
+    if request.method == 'POST':
+        form = FoodItemForm(request.POST, request.FILES, instance=food_item)
+        if form.is_valid():
+            form.save()
+            return redirect('food_menu:detail', item_id=food_item.id)
+    else:
+        form = FoodItemForm(instance=food_item)
+    context = {'form': form, 'food_item': food_item}
+    return render(request, 'food_menu/create_item.html', context)
+
+def delete_food_item(request, item_id):
+  food_item = FoodItem.objects.get(id=item_id)
+  if request.method == "POST":
+          food_item.delete()
+          return redirect('food_menu:index')
+  context = {"food_item": food_item}
+  return render(request, "food_menu/create_item.html", context)
