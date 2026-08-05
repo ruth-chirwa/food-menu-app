71fe7fdb6af4808208c521252ea1b3e86025011e
ruth-chirwa <chirwaruth647@gmail.com>
2026-08-05 11:20:05 +0200

    add image url

--- menu/food_menu/models.py
+++ menu/food_menu/models.py
@@ -5,6 +5,7 @@ class FoodItem(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.CharField(max_length=600, default="https://img.freepik.com/premium-psd/plate-food-with-variety-ingredients-transparent-background_679658-77415.jpg")
 
     def __str__(self):
         return f"{self.name} - MWK{self.price}"
