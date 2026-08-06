e50cc57336cda5630500a5924799f8b9268e3ee2
ruth-chirwa <chirwaruth647@gmail.com>
2026-08-06 16:09:26 +0200

    feat: add authentication app

--- menu/menu/settings.py
+++ menu/menu/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "food_menu.apps.FoodMenuConfig",
+    "authentication.apps.AuthenticationConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
