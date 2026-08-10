e64862dbe356ff2a5e768456a4969cf79ee36cce
ruth-chirwa <chirwaruth647@gmail.com>
2026-08-10 09:45:21 +0200

    added fields in form

--- menu/authentication/views.py
+++ menu/authentication/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from authentication.forms import RegistrationForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
-    context = {"form": form }
+    form = RegistrationForm()
+   
     if request.method == "POST":
-        form = UserCreationForm(request.POST, request.FILES)
+        form = RegistrationForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             user_name = form.cleaned_data.get("username")
             messages.success(request, f"Account created for {user_name}")
             return redirect("food_menu:index")
-        
+        else:
+            print(form.errors)
+    context = {"form": form }  
     return render(request, "authentication/register.html", context)
