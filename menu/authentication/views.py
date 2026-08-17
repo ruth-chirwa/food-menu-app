7babde7bcd410ad2af8444df7ea74b73e36fc786
ruth-chirwa <chirwaruth647@gmail.com>
2026-08-17 10:28:48 +0200

    feat: add logout function

--- menu/authentication/views.py
+++ menu/authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from authentication.forms import RegistrationForm
+from django.contrib.auth import logout
 
 # Create your views here.
 def register(request):
@@ -12,8 +13,11 @@ def register(request):
             form.save()
             user_name = form.cleaned_data.get("username")
             messages.success(request, f"Account created for {user_name}")
-            return redirect("food_menu:index")
+            return redirect("login")
         else:
             print(form.errors)
     context = {"form": form }  
     return render(request, "authentication/register.html", context)
+
+def logout_view(request):
+    logout(request)
