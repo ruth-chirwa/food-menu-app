162f7b6e1733df2bdcff9988b58e424ad232303d
ruth-chirwa <chirwaruth647@gmail.com>
2026-08-17 09:45:33 +0200

    add login url

--- menu/authentication/urls.py
+++ menu/authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 app_name = "authentication"
 
 urlpatterns = [
     path("register/", views.register, name="register"),
+    path("login/", auth_views.LoginView.as_view(), name="login"),
 ]
