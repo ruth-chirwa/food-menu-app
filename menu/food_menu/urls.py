c8985f8061455844bb08b99b673d64be5b954cc1
ruth-chirwa <chirwaruth647@gmail.com>
2026-08-17 15:55:06 +0200

    feat: add class based views

--- menu/food_menu/urls.py
+++ menu/food_menu/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "food_menu"
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('detail/<int:item_id>/', views.detail, name='detail'),
+    path('', views.IndexClassView.as_view(), name='index'),
+    path('detail/<int:pk>/', views.FoodDetailClassView.as_view(), name='detail'),
     path('add/', views.create_food_item, name='create_food_item'),
     path('update/<int:item_id>/', views.update_food_item, name='update_food_item'),
     path('delete/<int:item_id>/', views.delete_food_item, name='delete_food_item'),
--- menu/food_menu/views.py
+++ menu/food_menu/views.py
@@ -1,24 +1,35 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.template import context
+from django.views.generic.detail import DetailView
 from food_menu.models import FoodItem
 from food_menu.forms import FoodItemForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
+from django.views.generic.list import ListView
+# @login_required
+# def index(request):
+#     food_items = FoodItem.objects.all()
+#     context =  {"food_items": food_items}
+#     return render(request, "food_menu/menu.html", 
+#                     context
+#                   )
 
-@login_required
-def index(request):
-    food_items = FoodItem.objects.all()
-    context =  {"food_items": food_items}
-    return render(request, "food_menu/menu.html", 
-                    context
-                  )
+class IndexClassView(ListView):
+    model = FoodItem
+    template_name = 'food_menu/menu.html'
+    context_object_name =  'food_items'
 
-def detail(request, item_id):
-    food_item = FoodItem.objects.get(id=item_id)
-    context = {"food_item": food_item}
-    return render(request, "food_menu/detail.html", context)
 
+# def detail(request, item_id):
+#     food_item = FoodItem.objects.get(id=item_id)
+#     context = {"food_item": food_item}
+#     return render(request, "food_menu/detail.html", context)
+
+class FoodDetailClassView(DetailView):
+    model = FoodItem
+    template_name = 'food_menu/detail.html'
+    context_object_name = 'food_item'
 
 def create_food_item(request):
   if request.method == 'POST':
